b0a9e0f17968a4621434f29e92d0bd3b54acf097
YoonJunSik <junsik0616@chungbuk.ac.kr>
2024-12-13 01:14:43 +0900

    old file delete

--- demo/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.sbb.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List; // List를 임포트
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-    List<Answer> findByQuestionId(Integer questionId);
-}
